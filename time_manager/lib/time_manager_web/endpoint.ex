ce9f9ffb3e81e7f1113f0bc3c3c24ddfd88d6a8b
Oscar Robert-Besle <oscar.robert-besle@epitech.eu>
2024-10-17 13:28:44 +0200

    frankulin 31

--- time_manager/lib/time_manager_web/endpoint.ex
+++ time_manager/lib/time_manager_web/endpoint.ex
@@ -21,7 +21,7 @@ defmodule TimeManagerWeb.Endpoint do
   end
 
   plug CORSPlug, 
-    origin: ["https://vue.orbesle.fr:8080"],
+    origin: ["https://vue.orbesle.fr"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     headers: ["Authorization", "Content-Type"],
     expose: ["Authorization"]
