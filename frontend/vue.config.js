30579e2f10a739741823cda9c01e7c6d475548eb
Oscar Robert-Besle <oscar.robert-besle@epitech.eu>
2024-10-17 11:35:55 +0200

    frankulin 27

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',  // Écoute sur toutes les interfaces réseau
+    port: 8080,       // Le port sur lequel Vue.js doit écouter
+    allowedHosts: [
+      'vue.orbesle.fr'  // Accepte les requêtes provenant de ce domaine
+    ],
+    client: {
+      webSocketURL: 'wss://vue.orbesle.fr/ws',  // WebSocket pour Live Reload (HTTPS)
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Pour éviter les problèmes CORS
+    },
+  },
 })
