c1314a56f0b818ac4f0b4f365a0d2b7ec56dc2fd
LEKALIF <louis.salanoubat@epitech.eu>
2024-10-18 19:21:36 +0200

    restrict backend browser access

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,26 +1,21 @@
-const webpack = require('webpack');
-
-
 module.exports = {
   transpileDependencies: true,
 
   devServer: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: [
-      'web.orbesle.fr',
-      'localhost'
-    ],
+    host: '0.0.0.0',  // Bind to all IPs
+    port: 8080,       // Port where the app will run
+    allowedHosts: 'all',  // Allow all hosts to prevent "Invalid Host header"
     headers: {
       'Access-Control-Allow-Origin': '*', 
     },
     proxy: {
-      '/': {
-        target: process.env.VUE_APP_API_TARGET,  // Use correct domain based on environment
+      '/api': {
+        target: process.env.VUE_APP_API_TARGET,  // Proxy API requests
         changeOrigin: true,
       },
     },
     hot: false,
     liveReload: false,
+    disableHostCheck: true,  // Disable the host check to avoid the Invalid Host header issue
   },
 };
