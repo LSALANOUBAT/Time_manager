aaf617b80b8d90f0862df1b30a0de25f0a7be63e
LEKALIF <louis.salanoubat@epitech.eu>
2024-10-18 19:15:30 +0200

    restrict backend browser access

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,44 +1,26 @@
-const { defineConfig } = require('@vue/cli-service');
 const webpack = require('webpack');
 
-module.exports = defineConfig({
+
+module.exports = {
   transpileDependencies: true,
 
   devServer: {
     host: '0.0.0.0',
     port: 8080,
     allowedHosts: [
-      'vue.orbesle.fr',
-      'localhost:8080'
+      'web.orbesle.fr',
+      'localhost'
     ],
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': '*', 
     },
     proxy: {
       '/': {
-        target: process.env.VUE_APP_API_URL,
+        target: process.env.VUE_APP_API_TARGET,  // Use correct domain based on environment
         changeOrigin: true,
       },
     },
     hot: false,
     liveReload: false,
-
-    // Fix the client and webSocketURL options
-    client: {
-      webSocketURL: {
-        hostname: 'localhost',   // or 'vue.orbesle.fr' depending on your setup
-        port: 8080,              // port of your dev server
-        protocol: 'ws',          // or 'wss' if using SSL
-      },
-    },
   },
-
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        __VUE_PROD_DEVTOOLS__: false,
-        __VUE_OPTIONS_API__: true,
-      }),
-    ],
-  },
-});
+};
