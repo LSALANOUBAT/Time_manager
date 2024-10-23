55c1e395531d64dfcace8a1b75c71256af523c7c
Oscar Robert-Besle <oscar.robert-besle@epitech.eu>
2024-10-23 11:11:13 +0200

    correct logout fect problem

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,16 +13,34 @@ import { isAuthenticated } from './router/auth'; // Vérifie si l'utilisateur es
 
 export default {
   name: 'App',
-  computed: {
-    isAuthenticated() {
-      return isAuthenticated(); // Vérifie si l'utilisateur est connecté
+  data() {
+    return {
+      isAuth: isAuthenticated(), // Initialise l'état de l'authentification
+    };
+  },
+  watch: {
+    // Utilise un watcher pour détecter les changements de route
+    '$route'() {
+      this.updateAuthState();
     },
   },
   methods: {
     logout() {
       localStorage.removeItem('token'); // Supprime le token JWT
       this.$router.push('/login'); // Redirige vers la page de login après déconnexion
+      this.updateAuthState(); // Met à jour l'état de l'authentification
     },
+    updateAuthState() {
+      this.isAuth = isAuthenticated(); // Mets à jour la valeur de isAuth en fonction de l'état de l'authentification
+    },
+  },
+  computed: {
+    isAuthenticated() {
+      return this.isAuth; // Utilise la propriété isAuth
+    },
+  },
+  created() {
+    this.updateAuthState(); // Assure-toi que l'état est mis à jour au chargement du composant
   },
 };
 </script>
