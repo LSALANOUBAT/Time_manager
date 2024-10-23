507b505062d578c7cc78529390f1ec250c7020b3
Oscar Robert-Besle <oscar.robert-besle@epitech.eu>
2024-10-23 16:27:26 +0200

    added register

--- frontend/src/components/UserRegister.vue
+++ frontend/src/components/UserRegister.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="register-component">
-    <h2>Register</h2>
-    <form @submit.prevent="registerUser">
-      <input v-model="username" placeholder="Username" required />
-      <input v-model="email" type="email" placeholder="Email" required />
+    <form class="form" @submit.prevent="registerUser">
+      <p class="heading">Register</p>
+      <input v-model="username" class="input" placeholder="Username" required />
+      <input v-model="email" class="input" type="email" placeholder="Email" required />
       <input
-          v-model="password"
-          type="password"
-          placeholder="Password"
-          required
-          minlength="6"
+        v-model="password"
+        class="input"
+        type="password"
+        placeholder="Password"
+        required
+        minlength="6"
       />
-      <button type="submit">Register</button>
+      <button type="submit" class="btn">Register</button>
     </form>
 
     <div v-if="errorMessage" class="error-message">
@@ -79,7 +80,84 @@ export default {
 
 <style scoped>
 .register-component {
-  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f4f4f4;
+}
+
+.form {
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  background-color: white;
+  padding: 3em;
+  border-radius: 25px;
+  transition: .4s ease-in-out;
+  box-shadow: rgba(0, 0, 0, 0.4) 1px 2px 2px;
+  width: 350px;
+}
+
+.form:hover {
+  transform: translateX(-0.5em) translateY(-0.5em);
+  border: 1px solid #171717;
+  box-shadow: 10px 10px 0px #666666;
+}
+
+.heading {
+  color: black;
+  padding-bottom: 2em;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+
+.input {
+  border-radius: 5px;
+  border: 1px solid whitesmoke;
+  background-color: whitesmoke;
+  outline: none;
+  padding: 1em;
+  transition: .4s ease-in-out;
+  font-size: 1.1em;
+}
+
+.input:hover {
+  box-shadow: 6px 6px 0px #969696,
+             -3px -3px 10px #ffffff;
+}
+
+.input:focus {
+  background: #ffffff;
+  box-shadow: inset 2px 5px 10px rgba(0,0,0,0.3);
+}
+
+.form .btn {
+  margin-top: 2em;
+  align-self: center;
+  padding: 0.7em;
+  padding-left: 1em;
+  padding-right: 1em;
+  border-radius: 10px;
+  border: none;
+  color: black;
+  transition: .4s ease-in-out;
+  box-shadow: rgba(0, 0, 0, 0.4) 1px 1px 1px;
+  background-color: #f4f4f4;
+  font-size: 1.1em;
+}
+
+.form .btn:hover {
+  box-shadow: 6px 6px 0px #969696,
+             -3px -3px 10px #ffffff;
+  transform: translateX(-0.5em) translateY(-0.5em);
+}
+
+.form .btn:active {
+  transition: .2s;
+  transform: translateX(0em) translateY(0em);
+  box-shadow: none;
 }
 
 .error-message {
