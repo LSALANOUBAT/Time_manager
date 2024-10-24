823720c31d339b93e3c68e12109caa1c72f68dc1
UlysseTRIN <ulysse.trin@epitech.eu>
2024-10-24 16:12:54 +0200

    end worktimes not required in psql

--- time_manager/priv/repo/migrations/20241023120520_create_workingtimes.exs
+++ time_manager/priv/repo/migrations/20241023120520_create_workingtimes.exs
@@ -4,7 +4,7 @@ defmodule TimeManager.Repo.Migrations.CreateWorkingtimes do
   def change do
     create table(:workingtimes) do
       add :start, :utc_datetime, null: false
-      add :end, :utc_datetime,
+      add :end, :utc_datetime
       add :user_id, references(:users, on_delete: :delete_all), null: false
       add :night_time, :boolean, default: false, null: false
       timestamps(type: :utc_datetime)
